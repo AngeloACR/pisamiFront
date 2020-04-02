1fd031dd6fc452f35b8e0e0ff67949f98832b5ef
Angelo Crincoli <angelocrincoli91@gmail.com>
2020-04-02 16:14:13 -0400

    Agregando html al registro

--- pisamiUsers/src/app/components/registro/registro.component.ts
+++ pisamiUsers/src/app/components/registro/registro.component.ts
@@ -7,27 +7,34 @@ import { forkJoin } from 'rxjs';
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
-  styleUrls: ['./registro.component.scss'],
+  styleUrls: ['./registro.component.css'],
 })
 export class RegistroComponent implements OnInit {
 
   id: string;
 
   registroUser: FormGroup;
-  registroMusico: FormGroup;
+  registroSolista: FormGroup;
+  registroDuo: FormGroup;
+  registroOrquesta: FormGroup;
 
   formCompleted: Boolean;
   formSelected: Boolean;
   tipoSelected: String;
   isUser: Boolean;
-  isMusicoSolo: Boolean;
+  isMusico: Boolean;
+  isSolista: Boolean;
   isDuo: Boolean;
   isOrquesta: Boolean;
   showBlack: {};
   showForm: {};
+  hideOption: {};
   selectedImg: String;
   userImg: String;
   musicoImg: String;
+  solistaImg: String;
+  duoImg: String;
+  orquestaImg: String;
 
   constructor(
     private actRoute: ActivatedRoute,
@@ -50,6 +57,106 @@ export class RegistroComponent implements OnInit {
     });
   }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.isMusico = false;
+    this.initForm();
+    this.musicoImg = 'assets/REGISTRO/MUSICO.png';
+    this.userImg = 'assets/REGISTRO/USUARIO.png';
+    this.solistaImg = 'assets/TIPO DE PERFIL/CANTANTEMUSICO.png';
+    this.duoImg = 'assets/TIPO DE PERFIL/DUO.png';
+    this.orquestaImg = 'assets/TIPO DE PERFIL/ORQUESTA.png';
+  }
+
+  initForm() {
+    this.registroUser = new FormGroup({
+      nombre: new FormControl(''),
+      apellido: new FormControl(''),
+      tlf: new FormControl(''),
+      correo: new FormControl(''),
+      password: new FormControl(''),
+    });
+
+    this.registroSolista = new FormGroup({
+    });
+
+    this.registroOrquesta = new FormGroup({
+    });
+
+    this.registroOrquesta = new FormGroup({
+    });
+
+  }
+
+  toggleForm(event, tipo) {
+    this.formSelected = true;
+    let img;
+    switch (tipo) {
+      case 'user':
+        this.isUser = true;
+        this.isSolista = false;
+        this.isDuo = false;
+        this.isOrquesta = false;
+        img = this.userImg;
+        break;
+      case 'solista':
+        this.isUser = false;
+        this.isSolista = true;
+        this.isDuo = false;
+        this.isOrquesta = false;
+        img = this.solistaImg;
+        break;
+      case 'duo':
+        this.isUser = false;
+        this.isSolista = false;
+        this.isDuo = true;
+        this.isOrquesta = false;
+        img = this.duoImg;
+        break;
+      default:
+        this.isUser = false;
+        this.isSolista = false;
+        this.isDuo = false;
+        this.isOrquesta = true;
+        img = this.orquestaImg;
+        break;
+    }
+
+    this.selectedImg = img;
+    this.tipoSelected = tipo;
+    this.showForm = {
+      formAct: true
+    }
+    this.showBlack = {
+      blackAct: true
+    }
+
+    this.hideOption = {
+      hideOption: true
+    }
+  }
+
+  firstToggle(event, tipo) {
+    switch (tipo) {
+      case 'usuario':
+        this.toggleForm(event, 'user');
+        break;
+      default:
+        this.isMusico = true;
+        break;
+    }
+  }
+
+  closeForm() {
+    this.showForm = {
+      formAct: false
+    }
+    this.showBlack = {
+      blackAct: false
+    }
+
+    this.hideOption = {
+      hideOption: false
+    }
+  }
 
 }
