ef8d4067946fbb8428bd7d913b8f9d297049e0bd
Angelo Crincoli <angelocrincoli91@gmail.com>
2020-04-09 11:34:03 -0400

    Corrigiendo los headers y agregando form para crear generos musicales

--- pisamiAdmin/src/app/components/generos/generos.component.ts
+++ pisamiAdmin/src/app/components/generos/generos.component.ts
@@ -1,14 +1,72 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap, NavigationEnd } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-generos',
   templateUrl: './generos.component.html',
-  styleUrls: ['./generos.component.scss'],
+  styleUrls: ['./generos.component.css'],
 })
 export class GenerosComponent implements OnInit {
 
-  constructor() { }
+  id: string;
 
-  ngOnInit() {}
+  registroGenero: FormGroup;
+
+  isCrear: Boolean;
+  isListar: Boolean;
+
+  formCompleted: Boolean;
+  formSelected: Boolean;
+  tipoSelected: String;
+  showBlack: {};
+  showForm: {};
+  hideOption: {};
+
+  constructor(
+    private actRoute: ActivatedRoute,
+    private router: Router,
+    private fb: FormBuilder
+  ) {
+    this.actRoute.params.subscribe(params => {
+      this.id = params['id'];
+    });
+    this.router.events.subscribe(event => {
+      this.actRoute.url.subscribe(value => {
+        let url = value[0].path;
+        if (url == 'generos') {
+          console.log(this.id)
+          if (event instanceof NavigationEnd) {
+            this.ngOnInit();
+          }
+        }
+
+      });
+    });
+  }
+
+  ngOnInit() {
+    this.initForm();
+    if (this.id == '0') {
+      this.isCrear = true;
+      this.isListar = false;
+    } else if (this.id == '1') {
+      this.isCrear = false;
+      this.isListar = true;
+    }
+  }
+
+  initForm() {
+    this.registroGenero = new FormGroup({
+      id: new FormControl(''),
+      nombre: new FormControl(''),
+      descripcion: new FormControl(''),
+    });
+  }
+
+  endRegistro() {
+  }
 
 }
