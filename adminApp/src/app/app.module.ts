8c2276cecc3bcec9d26dc0da8e7f67384abbcaf3
Angelo Crincoli <angelocrincoli91@gmail.com>
2020-07-29 17:44:09 -0400

    Actualizando registro de musico en administrador, agregando splash de inicio

--- adminApp/src/app/app.module.ts
+++ adminApp/src/app/app.module.ts
@@ -1,46 +1,45 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouteReuseStrategy } from "@angular/router";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
-import { IonicStorageModule } from '@ionic/storage';
+import { IonicModule, IonicRouteStrategy } from "@ionic/angular";
+import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
+import { IonicStorageModule } from "@ionic/storage";
 
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { FormArtistasComponent } from './components/form-artistas/form-artistas.component';
-import { FormNotificacionesComponent } from './components/form-notificaciones/form-notificaciones.component';
-import { FormPoliticasComponent } from './components/form-politicas/form-politicas.component';
-import { FormGenerosComponent } from './components/form-generos/form-generos.component';
-import { FormUsuariosComponent } from './components/form-usuarios/form-usuarios.component';
-import { HeaderComponent } from './components/header/header.component';
-import { ListaComponent } from './components/lista/lista.component';
-import { ListaUsuariosComponent } from './components/lista-usuarios/lista-usuarios.component';
-import { ListaArtistasComponent } from './components/lista-artistas/lista-artistas.component';
-import { ListaGenerosComponent } from './components/lista-generos/lista-generos.component';
-import { ListaNotificacionesComponent } from './components/lista-notificaciones/lista-notificaciones.component';
-import { ListaPoliticasComponent } from './components/lista-politicas/lista-politicas.component';
-import { LoginComponent } from './components/login/login.component';
-import { ValidacionComponent } from './components/validacion/validacion.component';
-import { AdminComponent } from './components/admin/admin.component';
-import { RegistroUsuariosComponent } from './components/registro-usuarios/registro-usuarios.component';
-import { RegistroArtistasComponent } from './components/registro-artistas/registro-artistas.component';
-import { RegistroGenerosComponent } from './components/registro-generos/registro-generos.component';
-import { RegistroNotificacionesComponent } from './components/registro-notificaciones/registro-notificaciones.component';
-import { RegistroPoliticasComponent } from './components/registro-politicas/registro-politicas.component';
-import { RestorePassComponent } from './components/restore-pass/restore-pass.component';
-import { ResetPassComponent } from './components/reset-pass/reset-pass.component';
-import { EditarUsuariosComponent } from './components/editar-usuarios/editar-usuarios.component';
-import { EditarArtistasComponent } from './components/editar-artistas/editar-artistas.component';
-import { EditarGenerosComponent } from './components/editar-generos/editar-generos.component';
-import { EditarNotificacionesComponent } from './components/editar-notificaciones/editar-notificaciones.component';
-import { EditarPoliticasComponent } from './components/editar-politicas/editar-politicas.component';
-import { LogoutComponent } from './components/logout/logout.component';
-
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { FormArtistasComponent } from "./components/form-artistas/form-artistas.component";
+import { FormNotificacionesComponent } from "./components/form-notificaciones/form-notificaciones.component";
+import { FormPoliticasComponent } from "./components/form-politicas/form-politicas.component";
+import { FormGenerosComponent } from "./components/form-generos/form-generos.component";
+import { FormUsuariosComponent } from "./components/form-usuarios/form-usuarios.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { ListaComponent } from "./components/lista/lista.component";
+import { ListaUsuariosComponent } from "./components/lista-usuarios/lista-usuarios.component";
+import { ListaArtistasComponent } from "./components/lista-artistas/lista-artistas.component";
+import { ListaGenerosComponent } from "./components/lista-generos/lista-generos.component";
+import { ListaNotificacionesComponent } from "./components/lista-notificaciones/lista-notificaciones.component";
+import { ListaPoliticasComponent } from "./components/lista-politicas/lista-politicas.component";
+import { LoginComponent } from "./components/login/login.component";
+import { ValidacionComponent } from "./components/validacion/validacion.component";
+import { AdminComponent } from "./components/admin/admin.component";
+import { RegistroUsuariosComponent } from "./components/registro-usuarios/registro-usuarios.component";
+import { RegistroArtistasComponent } from "./components/registro-artistas/registro-artistas.component";
+import { RegistroGenerosComponent } from "./components/registro-generos/registro-generos.component";
+import { RegistroNotificacionesComponent } from "./components/registro-notificaciones/registro-notificaciones.component";
+import { RegistroPoliticasComponent } from "./components/registro-politicas/registro-politicas.component";
+import { RestorePassComponent } from "./components/restore-pass/restore-pass.component";
+import { ResetPassComponent } from "./components/reset-pass/reset-pass.component";
+import { EditarUsuariosComponent } from "./components/editar-usuarios/editar-usuarios.component";
+import { EditarArtistasComponent } from "./components/editar-artistas/editar-artistas.component";
+import { EditarGenerosComponent } from "./components/editar-generos/editar-generos.component";
+import { EditarNotificacionesComponent } from "./components/editar-notificaciones/editar-notificaciones.component";
+import { EditarPoliticasComponent } from "./components/editar-politicas/editar-politicas.component";
+import { LogoutComponent } from "./components/logout/logout.component";
+import { SplashComponent } from "./components/splash/splash.component";
 
 @NgModule({
   declarations: [
@@ -72,9 +71,9 @@ import { LogoutComponent } from './components/logout/logout.component';
     EditarNotificacionesComponent,
     RegistroNotificacionesComponent,
     EditarPoliticasComponent,
-    RegistroPoliticasComponent,   
-
-    ],
+    RegistroPoliticasComponent,
+    SplashComponent
+  ],
   entryComponents: [
     FormArtistasComponent,
     FormNotificacionesComponent,
@@ -83,16 +82,17 @@ import { LogoutComponent } from './components/logout/logout.component';
     FormUsuariosComponent,
     HeaderComponent,
     ListaComponent,
+    SplashComponent
   ],
   imports: [
-    BrowserModule, 
-    IonicModule.forRoot(), 
+    BrowserModule,
+    IonicModule.forRoot(),
     IonicStorageModule.forRoot(),
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule,
-    ],
+    HttpClientModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
--- usersApp/src/app/app.module.ts
+++ usersApp/src/app/app.module.ts
@@ -1,39 +1,39 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { CommonModule } from '@angular/common';  
-import { RouteReuseStrategy } from '@angular/router';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { CommonModule } from "@angular/common";
+import { RouteReuseStrategy } from "@angular/router";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
-import { IonicStorageModule } from '@ionic/storage';
+import { IonicModule, IonicRouteStrategy } from "@ionic/angular";
+import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
+import { IonicStorageModule } from "@ionic/storage";
 
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BuscadorComponent } from './components/buscador/buscador.component';
-import { DetalleArtistaComponent } from './components/detalle-artista/detalle-artista.component';
-import { FormArtistasComponent } from './components/form-artistas/form-artistas.component';
-import { FormBuscadorComponent } from './components/form-buscador/form-buscador.component';
-import { FormUsuariosComponent } from './components/form-usuarios/form-usuarios.component';
-import { HeaderComponent } from './components/header/header.component';
-import { LoginComponent } from './components/login/login.component';
-import { RatingComponent } from './components/rating/rating.component';
-import { RegistroComponent } from './components/registro/registro.component';
-import { VideosComponent } from './components/videos/videos.component';
-import { ResultadosBuscadorComponent } from './components/resultados-buscador/resultados-buscador.component';
-import { CancionesComponent } from './components/canciones/canciones.component';
-import { PerfilComponent } from './components/perfil/perfil.component';
-import { BuscadorVideosComponent } from './components/buscador-videos/buscador-videos.component';
-import { PoliticaComponent } from './components/politica/politica.component';
-import { BuscadorCancionesComponent } from './components/buscador-canciones/buscador-canciones.component';
-import { RestorePassComponent } from './components/restore-pass/restore-pass.component';
-import { ResetPassComponent } from './components/reset-pass/reset-pass.component';
-import { FavoritosComponent } from './components/favoritos/favoritos.component';
-import { WelcomeComponent } from './components/welcome/welcome.component';
-import { LogoutComponent } from './components/logout/logout.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BuscadorComponent } from "./components/buscador/buscador.component";
+import { DetalleArtistaComponent } from "./components/detalle-artista/detalle-artista.component";
+import { FormArtistasComponent } from "./components/form-artistas/form-artistas.component";
+import { FormBuscadorComponent } from "./components/form-buscador/form-buscador.component";
+import { FormUsuariosComponent } from "./components/form-usuarios/form-usuarios.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { LoginComponent } from "./components/login/login.component";
+import { RatingComponent } from "./components/rating/rating.component";
+import { RegistroComponent } from "./components/registro/registro.component";
+import { VideosComponent } from "./components/videos/videos.component";
+import { ResultadosBuscadorComponent } from "./components/resultados-buscador/resultados-buscador.component";
+import { CancionesComponent } from "./components/canciones/canciones.component";
+import { PerfilComponent } from "./components/perfil/perfil.component";
+import { BuscadorVideosComponent } from "./components/buscador-videos/buscador-videos.component";
+import { PoliticaComponent } from "./components/politica/politica.component";
+import { BuscadorCancionesComponent } from "./components/buscador-canciones/buscador-canciones.component";
+import { RestorePassComponent } from "./components/restore-pass/restore-pass.component";
+import { ResetPassComponent } from "./components/reset-pass/reset-pass.component";
+import { FavoritosComponent } from "./components/favoritos/favoritos.component";
+import { WelcomeComponent } from "./components/welcome/welcome.component";
+import { LogoutComponent } from "./components/logout/logout.component";
+import { SplashComponent } from "./components/splash/splash.component";
 
 @NgModule({
   declarations: [
@@ -43,10 +43,10 @@ import { LogoutComponent } from './components/logout/logout.component';
     FormArtistasComponent,
     FormBuscadorComponent,
     FormUsuariosComponent,
-    HeaderComponent, 
+    HeaderComponent,
     LoginComponent,
     PerfilComponent,
-    RatingComponent, 
+    RatingComponent,
     RestorePassComponent,
     ResetPassComponent,
     RegistroComponent,
@@ -58,8 +58,9 @@ import { LogoutComponent } from './components/logout/logout.component';
     BuscadorVideosComponent,
     FavoritosComponent,
     WelcomeComponent,
-    LogoutComponent
-    ],
+    LogoutComponent,
+    SplashComponent
+  ],
   entryComponents: [
     DetalleArtistaComponent,
     FormArtistasComponent,
@@ -67,22 +68,23 @@ import { LogoutComponent } from './components/logout/logout.component';
     FormUsuariosComponent,
     HeaderComponent,
     RatingComponent,
-    ResultadosBuscadorComponent,     
+    ResultadosBuscadorComponent,
     VideosComponent,
     CancionesComponent,
     BuscadorCancionesComponent,
     BuscadorVideosComponent,
+    SplashComponent
   ],
   imports: [
-    BrowserModule, 
-    IonicModule.forRoot(), 
+    BrowserModule,
+    IonicModule.forRoot(),
     IonicStorageModule.forRoot(),
     AppRoutingModule,
     FormsModule,
     CommonModule,
     ReactiveFormsModule,
-    HttpClientModule,
-    ],
+    HttpClientModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
